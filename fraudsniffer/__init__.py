6536808cfffc436f175c58d579c568a0efe24d87
boochix <dhruvameravanige@gmail.com>
2026-06-29 14:37:38 +0530

    i did an oopsie

--- fraudsniffer/__init__.py
+++
@@ -1,9 +0,0 @@
-"""FraudSniffer MVP package.
-
-Offline-first loan-underwriting document fraud detection built beside
-Quantum Sniffer's PQC and anomaly-detection foundations.
-"""
-
-from .pipeline import FraudSnifferService
-
-__all__ = ["FraudSnifferService"]
--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-
-# Add fraud_sniffer/ directory to sys.path so that tests can import 'fraudsniffer' package
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
